38242514f206c21fc669c3241de594c742a2914b
Austin Hill <hilaus@amazon.com>
2020-12-02 21:48:47 -0800

    Initial commit, why doesn't the mock return the correct number

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -2,14 +2,26 @@
 import XCTVapor
 
 final class AppTests: XCTestCase {
+    
+    let ADDRESS = "1111"
+    let SIGNATURE = "abcd"
+    let CANDIDATE = "JOHN DOE"
+    let QUANTITY = 22.22
+    
     func testHelloWorld() throws {
         let app = Application(.testing)
         defer { app.shutdown() }
         try configure(app)
-
-        try app.test(.GET, "hello", afterResponse: { res in
+        
+        let voteRequest = VoteRequest(id: ADDRESS, signature: SIGNATURE, candidate: CANDIDATE)
+        let voteResponse = Vote(voteRequest, quantity: QUANTITY)
+        
+        try app.test(.POST, "vote", beforeRequest: { req in
+            try req.content.encode(voteRequest)
+        }, afterResponse: { res in
             XCTAssertEqual(res.status, .ok)
-            XCTAssertEqual(res.body.string, "Hello, world!")
+            let returnedVote = try res.content.decode(Vote.self)
+            XCTAssertEqual(returnedVote, voteResponse)
         })
     }
 }
